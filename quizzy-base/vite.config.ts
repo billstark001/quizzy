e1d471dad4b68bd6b0acf63df6ad31f8a69cc1af
Bill Stark <31788509+BillStark001@users.noreply.github.com>
2024-05-19 18:55:34 +0900

    vite-tsconfig-paths

--- quizzy-base/vite.config.ts
+++ quizzy-base/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     dts({ include: ['lib'] }),
+    tsconfigPaths(),
   ],
   build: {
     copyPublicDir: false,
