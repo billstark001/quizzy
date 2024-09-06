09c7a964941445c0404e435bc4abee9b0429c7f4
Bill Stark <31788509+BillStark001@users.noreply.github.com>
2024-09-07 07:05:37 +0900

    *

--- quizzy-base/src/pages/EntryPage.tsx
+++ quizzy-base/src/pages/EntryPage.tsx
@@ -1,6 +1,4 @@
-import { TestPageTestBlank, TestPageTestChoice } from "@/test-pages";
 import { Button, HStack, VStack } from "@chakra-ui/react";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
@@ -16,23 +14,6 @@ export const EntryPage = () => {
       </Button>
     </HStack>
   </VStack>
-
-  return <TestPageTestBlank />;
-  // return <TestPageTestChoice />;
-  const [count, setCount] = useState(0);
-
-
-
-  return <>
-    <div>
-      <Button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </Button>
-      <p >
-        I18n test: {t('test')}
-      </p>
-    </div>
-  </>
 };
 
 
