701b441ac44a212a8a4dfe73b490a03205869c3e
Bill Stark <31788509+BillStark001@users.noreply.github.com>
2025-10-31 08:25:37 +0900

    *

--- quizzy-frontend/src/types/virtual-modules.d.ts
+++ quizzy-frontend/src/types/virtual-modules.d.ts
@@ -1,6 +1,7 @@
 declare module 'virtual:i18n-compiled' {
-  import type { CompiledLanguageResource } from '@/data/lang-model';
-  
+  export type CompiledLanguageResource = {
+    [key: string]: string | CompiledLanguageResource;
+  };
   export const langOrder: readonly string[];
   export const compiledLangRes: { [lang: string]: CompiledLanguageResource };
   export default compiledLangRes;
